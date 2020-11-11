ae7844617ba8084b9a738cfde3fa124ae01983ee
Dean Vo <deaanvo@gmail.com>
2020-11-10 18:04:15 -0800

    created data model

--- js/data.js
+++ js/data.js
@@ -1,1 +1,13 @@
 /* exported data */
+
+var data = {
+  view: 'home-page',
+  dealInfo: {
+    title: '',
+    normalPrice: '',
+    salePrice: '',
+    steamRating: '',
+    metacriticScore: '',
+    dealRating: ''
+  }
+};
